dae41e2f7b7226b023cbbc241a35ee0de173169c
linyunrui <yooryan@163.com>
2019-08-14 15:58:06 +0800

    init

---
+++ src/main/java/cn/lvji/ftpdemo/FtpDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.lvji.ftpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpDemoApplication.class, args);
+	}
+
+}
